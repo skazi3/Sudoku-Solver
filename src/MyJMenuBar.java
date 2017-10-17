852e1f10bd87e484f75ebbe43e933210504b29c4
sarah kazi <skazi3@uic.edu>
2017-10-17 00:09:06 -0500

    added action listener for load menu

--- src/MyJMenuBar.java
+++ src/MyJMenuBar.java
@@ -1,13 +1,25 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 
 public class MyJMenuBar extends JMenuBar{
 	private JMenuBar menuBar;
 	private JMenu fileMenu, helpMenu, hintsMenu; 
+	private JMenuItem loadMenuItem;
 	public MyJMenuBar() {
 		menuBar = new JMenuBar();
 		fileMenu = new JMenu("File");
 		helpMenu = new JMenu("Help");
 		hintsMenu = new JMenu("Hints");
+		
+		loadMenuItem = new JMenuItem("Load File");
+		
+		fileMenu.add(loadMenuItem);
+		loadMenuItem.addActionListener(new loadActionListener());
+		
 		menuBar.add(fileMenu);
 		menuBar.add(helpMenu); 
 		menuBar.add(hintsMenu);
@@ -16,4 +28,17 @@ public class MyJMenuBar extends JMenuBar{
 	public JMenuBar getMenuBar() {
 		return menuBar;
 	}
+	private class loadActionListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			JFileChooser fc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+			int returnVal = fc.showOpenDialog(null);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File selectedFile = fc.getSelectedFile();
+				System.out.println(selectedFile.getAbsolutePath());
+			}
+		}
+		
+	}
+	
 }
